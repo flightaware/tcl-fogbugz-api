f4f3102fea24dcc831528bd1f84cf63b738e3580
David McNett <nugget@flightaware.com>
2011-02-18 17:44:59 -0600

    Move to a generic getList proc. BUGZID:

--- main.tcl
+++ main.tcl
@@ -6,8 +6,12 @@ package require tls
 
 namespace eval ::fogbugz {
 
-set ::fields(ixPerson) [list ixPerson sFullName sEmail sPhone fAdministrator fCommunity fVirtual fDeleted fNotify sHomepage sLocale sLanguage sTimeZoneKey fExpert]
-set ::fields(interval) [list ixBug ixInterval dtStart dtEnd sTitle ixPerson]
+proc load_globals {} {
+	set ::fogbugz::fields(ixPerson) [list ixPerson sFullName sEmail sPhone fAdministrator fCommunity fVirtual fDeleted fNotify sHomepage sLocale sLanguage sTimeZoneKey fExpert]
+	set ::fogbugz::fields(interval) [list ixBug ixInterval dtStart dtEnd sTitle ixPerson]
+
+	set ::fogbugz::objType(People) ixPerson
+}
 
 proc get_xml {url qs} {
 	while {![info exists dh]} {
@@ -65,7 +69,7 @@ proc login {{api_url ""} {email ""} {password ""}} {
 }
 
 proc parse_element {element type} {
-	foreach field $::fields($type) {
+	foreach field $::fogbugz::fields($type) {
 		# debug "Parsing $element :: $field"
 		set value [[$element getElementsByTagName $field] asText]
 		dict set retdict $field $value
@@ -74,7 +78,36 @@ proc parse_element {element type} {
 	return $retdict
 }
 
-proc listPeople {dict} {
+proc getList {object dict} {
+	load_globals
+
+	set object [string totitle $object]
+	set qs [::http::formatQuery cmd "list$object" token [dict get $dict token]]
+	# puts "qs ::${qs}::"
+	lassign [get_xml $::fogbugz::config(api_url) $qs] success xml error
+
+	if {!$success} {
+		return [list 0 $error $xml]
+	}
+
+	# puts "-- \n$xml\n-- "
+	set dom [dom parse $xml]
+	set doc [$dom documentElement]
+	set nodeList [$doc selectNodes {/response/people/person}]
+	# puts "== \n$nodeList\n== "
+
+	set returnList [list]
+
+	foreach obj $nodeList {
+		lappend returnList [parse_element $obj $::fogbugz::objType($object)]
+	}
+
+	$dom delete
+
+	return $returnList
+}
+
+proc old_listPeople {dict} {
 	set qs [::http::formatQuery cmd listPeople token [dict get $dict token]]
 	lassign [get_xml $::fogbugz::config(api_url) $qs] success xml error
 
@@ -100,7 +133,7 @@ proc listPeople {dict} {
 }
 
 proc whoami {dict} {
-	set peopleList [listPeople $dict]
+	set peopleList [getList People $dict]
 
 	if {[info exists ::env(fogbugz_ixPerson)]} {
 		return $::env(fogbugz_ixPerson)
@@ -120,7 +153,7 @@ proc whoami {dict} {
 }
 
 
-proc listIntervals {dict} {
+proc old_listIntervals {dict} {
 	set qs [::http::formatQuery cmd listIntervals ixPerson [dict get $dict ixPerson] token [dict get $dict token] dtStart [dict get $dict dtStart]]
 	lassign [get_xml $::fogbugz::config(api_url) $qs] success xml error
 
