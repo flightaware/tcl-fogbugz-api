49bea34f1e980ec62e2200d449e8096414fb4d6f
David McNett <nugget@flightaware.com>
2011-02-20 10:14:20 -0600

    Modify the getList proc to use the new raw_cmd proc

--- main.tcl
+++ main.tcl
@@ -137,11 +137,7 @@ proc parse_element {element type} {
 }
 
 proc getList {object dict} {
-	set qs    [::http::formatQuery cmd "list$object"]
-	foreach arg [dict keys $dict] {
-		append qs "&[::http::formatQuery $arg [dict get $dict $arg]]"
-	}
-	lassign [get_xml $::fogbugz::config(api_url) $qs] success xml error
+	lassign [raw_cmd "list$object" $dict] success xml error
 
 	if {!$success} {
 		return [list 0 "getList $object ERROR: $error" $xml]
